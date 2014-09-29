b0a3a608fa1ffc905ff21363a94b65cca2cbd421
Heiko Stuebner <heiko@sntech.de>
2014-09-29 14:34:14 +0200

    bring rk808 regulator in line with devel/upstream

--- drivers/regulator/rk808-regulator.c
+++ drivers/regulator/rk808-regulator.c
@@ -3,8 +3,8 @@
  *
  * Copyright (c) 2014, Fuzhou Rockchip Electronics Co., Ltd
  *
- * Author: Chris Zhong<zyw@rock-chips.com>
- * Author: Zhang Qing<zhanqging@rock-chips.com>
+ * Author: Chris Zhong <zyw@rock-chips.com>
+ * Author: Zhang Qing <zhangqing@rock-chips.com>
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms and conditions of the GNU General Public License,
